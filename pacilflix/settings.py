5cd1bc8a4b6eb0865622e846c3c4daf277b07836
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-05-10 18:07:01 +0700

    Add DataBase To SupaBase

--- pacilflix/settings.py
+++ pacilflix/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'pacilflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'NAME': 'postgres',
+        'USER': 'postgres.ctcqmbdsxwgdpzsedrdl',
+        'PORT': '5432',
+        'PASSWORD': 'basdat12jaya',
     }
 }
 
