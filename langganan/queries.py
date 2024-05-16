526c9e2e971f7b3aa7e15e9365dad16493d3c813
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-05-16 20:05:07 +0700

    Implement Query for Beli and Bayar Feature.

--- langganan/queries.py
+++ langganan/queries.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import connection
 
 def daftar_paket():
@@ -22,10 +23,13 @@ def daftar_langganan(username):
             JOIN paket p ON t.nama_paket = p.nama
             JOIN pengguna pe ON t.username = pe.username
             JOIN dukungan_perangkat dp ON p.nama = dp.nama_paket
-            WHERE t.start_date_time < NOW() AND NOW() < t.end_date_time AND t.username = %s;
+            WHERE t.start_date_time < NOW() AND NOW() < t.end_date_time AND t.username = %s
+            ORDER BY t.start_date_time DESC
+            LIMIT 1;
         """, [username])
         result = cursor.fetchall()
         columns = [col[0] for col in cursor.description]
+        print(columns)
         return [dict(zip(columns, row)) for row in result]
     
 def daftar_transaksi(username):
@@ -36,9 +40,40 @@ def daftar_transaksi(username):
             FROM transaction t
             JOIN paket p ON t.nama_paket = p.nama
             JOIN pengguna pe ON t.username = pe.username
-            JOIN dukungan_perangkat dp ON p.nama = dp.nama_paket
             WHERE t.username = %s;
         """, [username])
         result = cursor.fetchall()
         columns = [col[0] for col in cursor.description]
         return [dict(zip(columns, row)) for row in result]
+
+    
+def get_paket(nama, dukungan_perangkat):
+    with connection.cursor() as cursor:
+        cursor.execute("SET search_path TO pacilflix")
+        cursor.execute("""
+            SELECT p.nama, p.harga, p.resolusi_layar, dp.dukungan_perangkat
+            FROM paket p
+            JOIN dukungan_perangkat dp ON p.nama = dp.nama_paket
+            WHERE p.nama = %s AND dp.dukungan_perangkat = %s;
+        """, [nama, dukungan_perangkat])
+        result = cursor.fetchall()
+        columns = [col[0] for col in cursor.description]
+        return [dict(zip(columns, row)) for row in result]
+
+def bayar_paket_post(username, nama, metode_pembayaran):
+    date_now = datetime.datetime.now()
+    end_date = date_now + datetime.timedelta(days=30)
+
+    date_now_timestamp_str = date_now.strftime('%Y-%m-%d %H:%M:%S')
+    date_now_str = date_now.strftime('%Y-%m-%d')
+    end_date_str = end_date.strftime('%Y-%m-%d')
+
+    # Asusmsi pada pembelian paket dengan durasi waktu 30 hari
+    with connection.cursor() as cursor:
+        cursor.execute("SET search_path TO pacilflix")
+        cursor.execute("""
+            INSERT INTO transaction (username, start_date_time, end_date_time, nama_paket, metode_pembayaran, timestamp_pembayaran)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """, [username,date_now_str, end_date_str, nama, metode_pembayaran, date_now_timestamp_str])
+
+    print("Berhasil membeli paket!")
