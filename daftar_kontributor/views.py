4ae06159f0a7b884eecfb6cd6caf063f9e490550
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-05-17 18:40:53 +0700

    Protect Endpoint for Daftar Kontributor

--- daftar_kontributor/views.py
+++ daftar_kontributor/views.py
@@ -1,8 +1,14 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .queries import *
 
 
 def show_daftar_kontributor(request):
+    try:
+        username = request.COOKIES['username']
+    except:
+        return HttpResponseRedirect(reverse("authentication:login"))
     choice = request.GET.get('filter_choice', None)
     daftar_kontributor = filter_kontirbutor(choice)
     return render(request, "daftar_kontributor.html", {'daftar_kontributor': daftar_kontributor})
