93bde248d4e2cca5a1360b6197ed8f391b96c7e0
eryawww <zazaneryawan@gmail.com>
2024-05-17 21:26:08 +0700

    refactor: adding app_name and endpoint namespace

--- tayangan/urls.py
+++ tayangan/urls.py
@@ -2,15 +2,17 @@ from django.urls import path
 
 from . import views
 
+app_name = 'tayangan'
+
 urlpatterns = [
-    path("list/", views.list),
-    path("list/<int:isGlobal>", views.list_filter),
-    path("search/<str:judul>", views.search),
-    path("film/<str:id>", views.film),
-    path("series/<str:id>", views.series),
-    path("episode/<str:id>", views.episode),
-    path('trailer/<int:isGlobal>', views.trailer_filter),
+    path("list/", views.list, name='list'),
+    path("list/<int:isGlobal>", views.list_filter, name='list_filtered'),
+    path("search/<str:judul>", views.search, name='search'),
+    path("film/<str:id>", views.film, name='film'),
+    path("series/<str:id>", views.series, name='series'),
+    path("episode/<str:id>", views.episode, name='episode'),
+    path('trailer/<int:isGlobal>', views.trailer_filter, name='trailer_filter'),
     path('trailer/', views.trailer, name = 'trailer'),
-    path("add_ulasan/", views.add_ulasan),
-    path("update_riwayat_nonton/", views.update_watchtime),
+    path("add_ulasan/", views.add_ulasan, name='ulasan'),
+    path("update_riwayat_nonton/", views.update_watchtime, name='update_riwayat'),
 ]
